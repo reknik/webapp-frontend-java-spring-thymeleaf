70272873bfbfbaa47fabea946652a7f0bc7e3e50
Alex <alexdicu09@yahoo.com>
2023-05-31 23:06:27 +0300

    - refactored exception handler

--- src/main/java/com/reknik/webAppDemoFrontEnd/config/AppConfig.java
+++ src/main/java/com/reknik/webAppDemoFrontEnd/config/AppConfig.java
@@ -1,25 +1,8 @@
 package com.reknik.webAppDemoFrontEnd.config;
 
-import com.reknik.webAppDemoFrontEnd.controller.RestTemplateErrorHandler;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
 
-  private final RestTemplateErrorHandler restTemplateErrorHandler;
-
-  @Autowired
-  public AppConfig(RestTemplateErrorHandler restTemplateErrorHandler) {
-    this.restTemplateErrorHandler = restTemplateErrorHandler;
-  }
-
-  @Bean
-  public RestTemplate getRestTemplate() {
-    RestTemplate restTemplate = new RestTemplate();
-    restTemplate.setErrorHandler(restTemplateErrorHandler);
-    return restTemplate;
-  }
 }
