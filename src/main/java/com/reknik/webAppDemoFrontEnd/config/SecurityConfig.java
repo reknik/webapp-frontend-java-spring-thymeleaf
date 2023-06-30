77cdd38019a43fc9df968d5b66b2963b91483f37
Dicu Florin <alexdicu09@yahoo.com>
2023-06-30 15:30:43 +0300

    - fixed deprecated methods

--- src/main/java/com/reknik/webAppDemoFrontEnd/config/SecurityConfig.java
+++ src/main/java/com/reknik/webAppDemoFrontEnd/config/SecurityConfig.java
@@ -6,11 +6,14 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestCustomizers;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.security.oauth2.client.web.server.DefaultServerOAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.HeaderWriterServerLogoutHandler;
+import org.springframework.security.web.server.header.ClearSiteDataServerHttpHeadersWriter;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -20,11 +23,9 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http,
                                                             ServerOAuth2AuthorizationRequestResolver resolver) {
-        http.authorizeExchange()
-                .anyExchange()
-                .authenticated()
-                .and()
-                .oauth2Login();
+        http.authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+                .oauth2Login(auth -> auth.authorizationRequestResolver(resolver))
+                .logout(logoutSpec -> logoutSpec.logoutHandler(new HeaderWriterServerLogoutHandler(new ClearSiteDataServerHttpHeadersWriter(ClearSiteDataServerHttpHeadersWriter.Directive.COOKIES))));
         return http.build();
     }
 
@@ -32,6 +33,7 @@ public class SecurityConfig {
     public ServerOAuth2AuthorizationRequestResolver pkceResolver(ReactiveClientRegistrationRepository repo) {
         DefaultServerOAuth2AuthorizationRequestResolver resolver =
                 new DefaultServerOAuth2AuthorizationRequestResolver(repo);
+        resolver.setAuthorizationRequestCustomizer(OAuth2AuthorizationRequestCustomizers.withPkce());
         return resolver;
     }
 
@@ -43,7 +45,7 @@ public class SecurityConfig {
                 new ServerOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrations, authorizedClients);
         oauth.setDefaultOAuth2AuthorizedClient(true);
         return WebClient.builder()
-                .baseUrl("http://localhost:8888/api")
+                .baseUrl("https://localhost:8888/api")
                 .filter(oauth)
                 .build();
     }
